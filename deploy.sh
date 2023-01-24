32f934fcee19f3f8e335f26b42084d15a81fad8c
Frank Leonardo Puma Chara <leonardo.chara15@gmail.com>
2023-01-24 17:34:07 -0500

    feat: edit index

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com: main <USERNAME >/ <USERNAME >.github.io.git
+# git push -f git@github.com: main <USERNAME >/ <USERNAME >.github.io.git
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:frankleonardopumachara/my-portfolio.git main:gh-pages
