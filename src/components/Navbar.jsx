1e216ae51fe67d3c16f1ba766db47c4b6df44366
Frank Leonardo Puma Chara <leonardo.chara15@gmail.com>
2023-02-10 21:34:09 -0500

    reactor: updated images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,12 +74,12 @@ export const Navbar = () => {
 
                         <div className='flex flex-shrink-0 items-center'>
                             <img className='block h-8 w-auto lg:hidden'
-                                 src='../../docs/assets/icons8-consola-24.png'
+                                 src='/docs/assets/icons8-consola-24.png'
                                  alt='terminal logo'
                             />
 
                             <img className='hidden h-8 w-auto lg:block'
-                                 src='../../docs/assets/icons8-consola-24.png'
+                                 src='/docs/assets/icons8-consola-24.png'
                                  alt='terminal logo'
                             />
                         </div>
